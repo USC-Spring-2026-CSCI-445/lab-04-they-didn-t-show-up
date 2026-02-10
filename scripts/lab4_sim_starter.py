81a2f97fbcf2bc1ea7f34f84aa945d4862dfd261
Ryder Baez <rbbaez@usc.edu>
2026-02-10 11:31:17 -0800

    z

--- scripts/lab4_sim_starter.py
+++ scripts/lab4_sim_starter.py
@@ -119,7 +119,7 @@ class RobotController:
             uRota = self.PconRota.control(self.desired_distance - self.ir_distance, time())
             
             ctrl_msg.linear.x = uLin
-            ctrl_msg.angular.x = uRota
+            ctrl_msg.angular.z = uRota
             ######### Your code ends here #########
 
             self.robot_ctrl_pub.publish(ctrl_msg)
