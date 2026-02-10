37cc69881fd4c8f56869bc7be0cfdbee72469fea
Ryder Baez <rbbaez@usc.edu>
2026-02-10 11:25:11 -0800

    d

--- scripts/lab4_sim_starter.py
+++ scripts/lab4_sim_starter.py
@@ -123,7 +123,7 @@ class RobotController:
             ######### Your code ends here #########
 
             self.robot_ctrl_pub.publish(ctrl_msg)
-            print(f"dist: {round(self.ir_distance, 4)}\ttgt: {round(self.desired_distance, 4)}\tu: {round(u, 4)}")
+            print(f"dist: {round(self.ir_distance, 4)}\ttgt: {round(self.desired_distance, 4)}\tu: {round(uLin, 4)}")
             rate.sleep()
 
 
